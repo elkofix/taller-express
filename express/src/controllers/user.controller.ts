e893cf2487e5e98b778142b9371ae61c3c09b457
elkofix <holasoygermamwow@gmail.com>
2025-03-29 10:20:46 -0500

    test: adding unit test for user controller and connectionDB

--- express/src/controllers/user.controller.ts
+++ express/src/controllers/user.controller.ts
@@ -29,45 +29,11 @@ class UserController{
             return;
         }catch(error){
             console.log(error);
-            throw error;
+            res.status(500).json(`cannot get the users`)
+            return;
         }
     }
 
-    async login(req: Request, res: Response){
-        /*
-        recibir usuario y contraseña
-        buscar que exista el usuario
-        comparar las contraseñas (vieja y la actual)
-        generar un token en caso de login exitoso. 
-        */
-       try{
-            const user:UserDocument | null = await userService.findByEmail(req.body.email);
-            if(!user){
-                res.status(400).json({ message: `user ${req.body.email} not found.`});
-                return;
-            }
-            const isMatch = await securityService.comparePasswords(req.body.password, user.password);
-
-            if(!isMatch){
-                res.status(400).json({
-                    message: `User or password incorrect`
-                });
-                return;
-            }
-            const token = await securityService.generateToken(user.id, user.email, user.role);
-            res.status(200).json({
-                message: "login successfull",
-                token: token
-            })
-            return;
-
-       }catch(error){
-        res.status(500).json({
-            message: "Login incorrect"
-        })
-        return;
-       }
-    }
 
     async update(req: Request, res: Response){
         try{
@@ -80,7 +46,7 @@ class UserController{
             res.json(user);
             return;
         }catch(error){
-            res.status(500).json({message: `The user ${req.body.email} cannot be updated.`})
+            res.status(500).json({message: `The user ${req.params.email} cannot be updated.`})
             return;
         }
     }
